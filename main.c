1c58ed81f1169dc14357e35f9206c3cd182b7e1d
unknown <137359388+Alizaaaja4@users.noreply.github.com>
2024-11-22 10:35:15 +0700

    add fitur login admin

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+    int angka[50], x, i;
+    printf("Banyaknya Data: "); scanf("%d", &x);
+    for(i = 0;i < x;i++){//(A) Tentukan operator batas perulangan
+        printf("Masukkan data ke-%d: ",i+1); scanf("%d", &angka[i]);//(B) Tentukan Variabel array
+    }
+    printf("Data: ");
+    for(i = 0;i < x;i++){
+        printf("%d", angka[i]);//(C) Tentukan format tipe data; (D) Tentukan variabel
+    }
+}
